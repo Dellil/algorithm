86bcf552af82f1dae27ee86bc04bae2ba1ff72ba
Dellil <aksaksdm@naver.com>
2022-03-11 15:29:25 +0900

    Update: 코드 수정

--- baekjoon/greedy/1417.js
+++ baekjoon/greedy/1417.js
@@ -1,28 +1,62 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-const input = fs
-  .readFileSync("/dev/stdin")
-  .toString()
-  .split("\n")
-  .map(v => v.replace('\r', ''))
-  .map(v => parseInt(v));
+// const input = fs
+//   .readFileSync("/dev/stdin")
+//   .toString()
+//   .split("\n")
+//   .map(v => v.replace('\r', ''))
+//   .map(v => parseInt(v));
 
-const candidateScoreList = input.slice(2, input.length);
+// const candidateScoreList = input.slice(2, input.length);
 
-if(input[0] === 1 ||input[1] > Math.max(...candidateScoreList)) {
-    console.log(0);
-    return;
-}
+// if(input[0] === 1 ||input[1] > Math.max(...candidateScoreList)) {
+//     console.log(0);
+// } else {
+//   let dasomScore = input[1];
+//   let minimumCount = 0;
+//   let maxCandidateScoreIndex = candidateScoreList.findIndex(v => v === Math.max(...candidateScoreList));
 
-let dasomScore = input[1];
-let minimumCount = 0;
-let maxCandidateScoreIndex = candidateScoreList.findIndex(v => v === Math.max(...candidateScoreList));
+//   while(dasomScore <= candidateScoreList[maxCandidateScoreIndex]) {
+//       dasomScore++;
+//       minimumCount++;
+//       candidateScoreList[maxCandidateScoreIndex]--;
+//       maxCandidateScoreIndex = candidateScoreList.findIndex(v => v === Math.max(...candidateScoreList));
+//   }
 
-while(dasomScore <= candidateScoreList[maxCandidateScoreIndex]) {
-    dasomScore++;
-    minimumCount++;
-    candidateScoreList[maxCandidateScoreIndex]--;
-    maxCandidateScoreIndex = candidateScoreList.findIndex(v => v === Math.max(...candidateScoreList));
-}
+//   console.log(minimumCount);
+// }
 
-console.log(minimumCount);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input.push(line)
+})
+  .on('close', function () {
+  input = input.map(v => parseInt(v));
+  const candidateScoreList = input.slice(2, input.length);
+
+  if(input[0] === 1 ||input[1] > Math.max(...candidateScoreList)) {
+      console.log(0);
+  } else {
+    let dasomScore = input[1];
+    let minimumCount = 0;
+    let maxCandidateScoreIndex = candidateScoreList.findIndex(v => v === Math.max(...candidateScoreList));
+  
+    while(dasomScore <= candidateScoreList[maxCandidateScoreIndex]) {
+        dasomScore++;
+        minimumCount++;
+        candidateScoreList[maxCandidateScoreIndex]--;
+        maxCandidateScoreIndex = candidateScoreList.findIndex(v => v === Math.max(...candidateScoreList));
+    }
+  
+    console.log(minimumCount);
+  }
+  process.exit();
+});
