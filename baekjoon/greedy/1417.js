6dbf79d458db314bf190394e9e26551f8690d8e5
Dellil <aksaksdm@naver.com>
2022-03-11 15:34:46 +0900

    Update: 1417번 문제 설명 업데이트

--- baekjoon/greedy/1417.js
+++ baekjoon/greedy/1417.js
@@ -48,15 +48,22 @@ rl.on('line', function (line) {
     let dasomScore = input[1];
     let minimumCount = 0;
     let maxCandidateScoreIndex = candidateScoreList.findIndex(v => v === Math.max(...candidateScoreList));
-  
+
     while(dasomScore <= candidateScoreList[maxCandidateScoreIndex]) {
         dasomScore++;
         minimumCount++;
         candidateScoreList[maxCandidateScoreIndex]--;
         maxCandidateScoreIndex = candidateScoreList.findIndex(v => v === Math.max(...candidateScoreList));
     }
-  
+
     console.log(minimumCount);
   }
   process.exit();
 });
+
+/**
+ * 2022/03/11
+ * node js 채점 환경때문에 억까당했었던 문제
+ * fs모듈이 아닌 readline을 활용해 입력을 받으면 채점이 오류 없이 잘된다.
+ * 먼저 최다 표를 받은 후보의 유권자를 매수하기 때문에 greedy하다.
+ */
