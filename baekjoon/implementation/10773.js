114e95365766d7a27ccb6559db32bed0a42f83c0
Dellil <aksaksdm@naver.com>
2022-03-24 18:48:02 +0900

    Add: 10773번 문제 추가

---
+++ baekjoon/implementation/10773.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', line => {
+    input.push(line);
+})
+.on('close', () => {
+    input = input.slice(1, input.length);
+
+    const stack = [];
+
+    input.forEach(v => {
+        if(v === '0') {
+            stack.pop();
+        } else stack.push(v);
+    });
+
+    const result = stack.reduce((acc, cur) => acc + parseInt(cur), 0);
+
+    console.log(result);
+});
